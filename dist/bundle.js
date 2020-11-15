3f22d066fedd9bcf4635e73210e5ee4f0640eac5
reidomath <mat.s.ferreira@gmail.com>
2020-11-15 18:56:44 -0300

    criando arquivos base para projeto - tsc, js e webpack

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+
+console.log("Hello World 4patinhas");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly80cGF0aW5oYXMvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFhO0FBQ2IiLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XHJcbmNvbnNvbGUubG9nKFwiSGVsbG8gV29ybGQgNHBhdGluaGFzXCIpO1xyXG4iXSwic291cmNlUm9vdCI6IiJ9
